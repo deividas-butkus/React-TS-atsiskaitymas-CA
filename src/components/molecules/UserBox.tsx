ac3a526a4e1698cacf2ee679467c337b9a30a163
deividas-butkus <deividas.butkus@hotmail.com>
2024-09-05 15:31:32 +0300

    div for logout changed to button to make it more semantically correct.

--- src/components/molecules/UserBox.tsx
+++ src/components/molecules/UserBox.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
+import Avatar from "../atoms/Avatar";
 
 const StyledUserBoxDiv = styled.div`
   display: flex;
@@ -21,23 +22,36 @@ const StyledUserBoxDiv = styled.div`
       color: #d73f03;
     }
   }
+  button {
+    background-color: transparent;
+    border: none;
+    font-family: inherit;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
 `;
 
 const UserBox = () => {
   return (
     <StyledUserBoxDiv>
+      <Link to={"/myFavorites"}>
+        <Avatar />
+        <span>Frank</span>
+      </Link>
       <Link to={"/login"}>
-        <LoginIcon />
+        <LoginIcon fontSize="small" />
         <span>Prisijungti</span>
       </Link>
       <Link to={"/register"}>
-        <HowToRegIcon />
+        <HowToRegIcon fontSize="small" />
         <span>Registruotis</span>
       </Link>
-      <div>
+      <button>
         <span>Atsijungti</span>
-        <LogoutIcon />
-      </div>
+        <LogoutIcon fontSize="small" />
+      </button>
     </StyledUserBoxDiv>
   );
 };
